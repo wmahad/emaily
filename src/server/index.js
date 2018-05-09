fd27274246dbef6ce0680f0045008c8f1f189b0d
Mahad Walusimbi <engmk123@gmail.com>
2018-05-10 01:34:11 +0300

    Change test here

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({'text': 'we here baby'});
+    res.send({text: 'Testing again'});
 });
 
 app.listen(PORT, () => {
