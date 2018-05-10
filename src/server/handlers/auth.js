fed355b962d296e947f6fcd689ae8f7855a91561
Mahad Walusimbi <engmk123@gmail.com>
2018-05-10 11:36:44 +0300

    Refactor server code

---
+++ src/server/handlers/auth.js
@@ -0,0 +1,7 @@
+module.exports = (app, passport) => {
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    app.get('/auth/google/callback', passport.authenticate('google'))
+}
--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('./services/passport');
+
 const PORT = process.env.PORT || 5000;
 
 // create express instance
@@ -8,6 +10,9 @@ app.get('/', (req, res) => {
     res.send({text: 'Testing again'});
 });
 
+// routes go here
+require('./handlers/auth')(app, passport);
+
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}...`)
+    console.log(`listening on port ${PORT}...`);
 });
